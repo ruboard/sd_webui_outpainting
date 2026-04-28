d5e6961041712550e6262b6f815c166376cc90c7
ruboard <79255903+ruboard@users.noreply.github.com>
2026-04-28 13:20:04 +0200

    Update core.py

--- scripts/core.py
+++ scripts/core.py
@@ -14,7 +14,7 @@ class ImageExtension():
     DEFAULT_MODEL = "sd2-community/stable-diffusion-2-inpainting"
     cache_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models")
 
-    # cache_dir = "F:/huggingface_model/"  # diffusers的本地缓存路径
+    # cache_dir = "F:/huggingface_model/"  # Local cache path for diffusers
 
     def __init__(self,
                  sampleTimeStep: int = 30,
